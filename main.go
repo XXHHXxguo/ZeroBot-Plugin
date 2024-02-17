b5f49c6eee55a1e252d7bfaa4b5116a1146c0d4b
XXHHXxguo <59596865+XXHHXxguo@users.noreply.github.com>
2024-02-17 21:15:40 +0800

    Update main.go

--- main.go
+++ main.go
@@ -287,7 +287,7 @@ func init() {
 	}
 	config.W = []*driver.WSClient{driver.NewWebSocketClient(*url, *token)}
 	config.Z = zero.Config{
-		NickName:       append([]string{*adana}, "ATRI", "atri", "亚托莉", "アトリ"),
+		NickName:       append([]string{*adana}, "Guinevere", "guinevere", "小桂子", "桂乃芬"),
 		CommandPrefix:  *prefix,
 		SuperUsers:     sus,
 		RingLen:        *rsz,
